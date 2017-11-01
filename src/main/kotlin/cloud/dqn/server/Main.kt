e4a88fe1721e422340de69c17e253034e369b28d
Dan Quoc Nguyen <aviuswen@gmail.com>
2017-10-31 22:10:24 -0700

    Undertow Hello World webserver and dependency in pom

--- src/main/kotlin/cloud/dqn/server/Main.kt
+++ src/main/kotlin/cloud/dqn/server/Main.kt
@@ -1,7 +1,17 @@
 package cloud.dqn.server
 
+import io.undertow.Undertow
+import io.undertow.util.Headers
+
 object Main {
     @JvmStatic fun main(args: Array<String>) {
-        println("I will run when jar file is packaged and used")
+        println("Running undertow webserver")
+        val server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler { exchange ->
+                    exchange.responseHeaders.put(Headers.CONTENT_TYPE, "text/plain")
+                    exchange.responseSender.send("Hello World Again")
+                }.build()
+        server.start()
     }
 }
